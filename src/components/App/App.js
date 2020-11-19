bb3c085ebe84a3da89cdc45dd5b80c84a23b8f42
Kamil Mrowka <kamil.mrowka@outlook.com.au>
2020-11-19 12:08:08 +0100

    add component Hero

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import List from '../List/List.js';
+import styles from './App.scss';
+import Hero from '../Hero/Hero.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className={styles.component}>
+        <h1 className={styles.title}>My first React app</h1>
+        <h2 className={styles.subtitle}>Hello world :-)</h2>
+        <List />
+      </main>
+    )
+  }
+}
+
+export default App;

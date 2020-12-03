dd04611bea08d9cc5946d61bf5fe95fb446ffaf5
Kamil Mrowka <kamil.mrowka@outlook.com.au>
2020-12-03 18:08:21 +0100

    latest version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink, Link} from 'react-router-dom';
 import style from './Header.scss';
 import Container from '../Container/Container';
+import Icon from './Icon';
 
 class Header extends React.Component {
 
@@ -11,10 +12,14 @@ class Header extends React.Component {
       <header className={style.component}>
         <Container>
           <div className={style.wrapper}>
-            <Link className={style.logo}>
-                
+            <Link className={style.logo} to='/'> 
+              <Icon />
             </Link>
-          </div>
+            <nav>
+              <NavLink exact to='/' activeClassName='active'>Home</NavLink>
+              <NavLink exact to='/info' activeClassName='active'>Info</NavLink> 
+            </nav>
+          </div> 
         </Container>
       </header>
     );
---
+++ src/components/Header/Icon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prop-types
+const Icon = () => (<i className="fas fa-universal-access"></i>
+);
+
+export default Icon;
